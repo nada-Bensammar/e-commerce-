0da7f50af6eb4e61ca289f6be608778d24b24b57
Malak-rtj <goudjilmalak04@gmail.com>
2025-03-22 23:01:52 +0100

    Update productController.js

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,5 +1,13 @@
 const Product = require('../backend/models/Product');
 
+exports.createProduct = async (req, res) => {
+  try {
+    const newProduct = await Product.create(req.body);
+    res.status(201).json(newProduct);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
 exports.getAllProducts = async (req, res) => {
   try {
     const products = await Product.getAll();
@@ -16,6 +24,18 @@ exports.getProductById = async (req, res) => {
       return res.status(404).json({ error: 'Product not found' });
     }
     res.json(product);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.deleteProduct = async (req, res) => {
+  try {
+    const deletedProduct = await Product.delete(req.params.id);
+    if (!deletedProduct) {
+      return res.status(404).json({ error: 'Product not found' });
+    }
+    res.status(204).json({ message: 'Product deleted successfully' });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
