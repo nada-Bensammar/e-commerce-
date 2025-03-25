585d2dbe8ce3a524c59ee06c5df54e7c2b6fbf89
Malak-rtj <goudjilmalak04@gmail.com>
2025-03-25 21:11:00 +0100

    Create comment.js

---
+++ backend/models/comment.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+  productId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Product',
+    required: true
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Comment', commentSchema);
