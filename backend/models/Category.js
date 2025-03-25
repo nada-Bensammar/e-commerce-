d92e649673fcf1725cb2c09ccff0ddd06695d531
nada <edrgb2021@gmail.com>
2025-03-25 15:47:38 +0100

    Update Category.js

--- backend/models/Category.js
+++ backend/models/Category.js
@@ -1,1 +1,54 @@
 
+
+const slugify = require('slugify');
+
+const categorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please add a category name'],
+    unique: true,
+    trim: true,
+    maxlength: [50, 'Name cannot be more than 50 characters']
+  },
+  slug: {
+    type: String,
+    unique: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  featured: {
+    type: Boolean,
+    default: false
+  },
+  image: {
+    type: String,
+    default: 'no-photo.jpg'
+  }
+}, {
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true }
+});
+
+
+categorySchema.pre('save', function(next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+
+categorySchema.pre('remove', async function(next) {
+  await mongoose.model('Product').deleteMany({ category: this._id });
+  next();
+});
+
+
+categorySchema.virtual('products', {
+  ref: 'Product',
+  localField: '_id',
+  foreignField: 'category',
+  justOne: false
+});
+
+module.exports = mongoose.model('Category', categorySchema);
